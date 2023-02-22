e73542810d6baa0ef12fc83b9d05c2b48bc0fd98
«Andreu <andreump5@gmail.com>
2023-02-22 20:16:56 +0100

    Començant el v111

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<x-layout>
+    <div class="py-10">
+        <header>
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold leading-tight text-gray-900">
+                    ABOUT
+                </h1>
+            </div>
+        </header>
+        <main>
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-10">
+                <ul>
+                    <li>Nom: Andreu Monforte Piñana</li>
+                    <li>Curs: ASIX 2</li>
+                    <li>Modul: M_09 Llengüatge de marques</li>
+                    <li>Unitat formativa 1</li>
+
+                </ul>
+
+                <p class="mt-10">Consulteu els screencasts del canal de Youtube acacha_dev per aprendre tots els passos des de zero per a crear aquesta web que esteu veient</p>
+                tempora vel voluptas.</p>
+            </div>
+        </main>
+</x-layout>
