4f3f24394e79ec6ae34ee678364fe9fe32d30d9d
«Andreu <andreump5@gmail.com>
2023-03-07 12:17:33 +0100

    v112  refactoritzant el web.php per a tenir la pagina de contact i about en controladors

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,16 @@
 
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TaskController::class,'index']);
+
 Route::get('/tasks',[TaskController::class,'index']);
 
 Route::get('/users', [UserController::class,'index']);
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', [PagesController::class,'contact']);
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', [PagesController::class,'about']);
 
